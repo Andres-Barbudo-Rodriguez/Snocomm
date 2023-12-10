fbe4443f1a07551f3a159369e552fb12acf88ea0
System Administrator <root@Andress-MacBook-Pro.local>
2023-12-10 18:15:54 -0300

    SSL/TLS Cipher Enumeration

---
+++ Snocomm/Snocomm/SSL-TLS Cipher Suite Enumeration.c
@@ -0,0 +1,15 @@
+//
+//  SSL-TLS Cipher Suite Enumeration.c
+//  Snocomm
+//
+//  Created by Andres Barbudo on 12/10/23.
+//  Copyright © 2023 Snocomm. All rights reserved.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+    system("nmap --script ssl-enum-ciphers -p 443 /.https && /.http");
+    return 0;
+}
