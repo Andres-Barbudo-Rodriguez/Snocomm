b028a4e3bf17d3c57518c9b51688ad18f04d713d
Andres Barbudo-Rodriguez <andresbarbudo@icloud.com>
2024-01-13 19:13:26 -0300

    c.l.u.

--- Snocomm/Snocomm/Codified Likeness Utility/python utilities/10 - network.zsh
+++ Snocomm/Snocomm/Codified Likeness Utility/python utilities/10 - network.zsh
@@ -2,3 +2,4 @@ sshd-generate
 service ssh start
 python "09 - network.py"
 python "12 - network.py" -H dictionary.txt -u  root -F dictionary.txt
+ssh user@host -i keyfile -o PasswordAuthentication=no
