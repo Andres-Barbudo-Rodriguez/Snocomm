c1662221e481f2c7e8d3961c6b6a5aede167b6c3
Andres Barbudo-Rodriguez <andresbarbudo@icloud.com>
2024-01-15 20:10:41 -0300

    c.l.u.

--- Snocomm/Snocomm/Codified Likeness Utility/python utilities/10 - network.zsh
+++ Snocomm/Snocomm/Codified Likeness Utility/python utilities/10 - network.zsh
@@ -3,3 +3,4 @@ service ssh start
 python "09 - network.py"
 python "12 - network.py" -H dictionary.txt -u  root -F dictionary.txt
 ssh user@host -i keyfile -o PasswordAuthentication=no
+nvram -d 'https://github.com/palera1n/PongoOS/blob/iOS15/scripts/pongoterm.c'
