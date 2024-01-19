381de61ed9eef5f0111f7b4b98ff06edafd0f02c
Andres Barbudo-Rodriguez <andresbarbudo@icloud.com>
2024-01-19 18:48:26 -0300

    network - 1.1.2
    system bundle encryption into the boot system

--- Snocomm/Snocomm/Codified Likeness Utility/network/01.sh
+++ Snocomm/Snocomm/Codified Likeness Utility/network/01.sh
@@ -1,2 +1,9 @@
 openssl aes-128-ecb -salt -in boot.efi -out nfs.encrypted -pass pass:30302E32 2E3978
 openssl aes-128-ecb -d -salt -in nfs-encrypted -out boot.efi -pass pass:30302E32 2E3978
+
+sourcedir="/usr/bin"
+encrfile="boot.efi.tar.gz"
+password=30302E32 2E3978
+
+tar czvf - "$sourcedir" | openssl des3 -salt -out "$encrfile" -pass pass:"$password"
+openssl des3 -d -salt -in "$encrfile" -pass pass:"$password" | tar xzv
