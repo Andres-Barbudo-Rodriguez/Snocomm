f8b6f2a1af5531c8dcc983ec821ab9e9a42d27d2
Andres Barbudo-Rodriguez <andresbarbudo@icloud.com>
2024-01-29 15:58:55 -0300

    network - 0.10
    sincronizado del protocolo de reloj de red

---
+++ Snocomm/Snocomm/Codified Likeness Utility/network/10.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import socket
+import struct
+import sys
+import time
+
+SERVIDOR_NTP =  "0.uk.pool.ntp.org"
+EPOCH = 2208988800
+
+def clienteSNTP():
+    cliente = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    datos = '\x1b' + 47 * '\0'
+    cliente.sendto(datos.encode('utf-8'),(SERVIDOR_NTP, 123))
+    datos, direccion = cliente.recvfrom(1024)
+    if datos:
+        print('respuesta recibida desde:', direccion)
+    
+    t = struct.unpack('!12I', datos)[10]
+    t -= EPOCH
+    print('\tTiempo=%s' % time.ctime(t))
+
+if __name__ == '__main__':
+    clienteSNTP()
