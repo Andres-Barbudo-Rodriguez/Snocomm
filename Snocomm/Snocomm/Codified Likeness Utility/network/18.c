e328ff88373d08defd77d1d99debc8b0d9830294
Andres Barbudo-Rodriguez <andresbarbudo@icloud.com>
2024-02-05 16:01:30 -0300

    network 2.18 - Configuración Inicial de Socket

---
+++ Snocomm/Snocomm/Codified Likeness Utility/network/18.c
@@ -0,0 +1,15 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <errno.h>
+
+
+#include <stdio.h>
+
+int main() {
+	printf("Listo para utilizar la Interface Programable de Aplicacion de Socket.\n");
+	return 0;
+}
