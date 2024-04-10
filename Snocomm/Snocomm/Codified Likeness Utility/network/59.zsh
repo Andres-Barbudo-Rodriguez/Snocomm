ff61a06bdd4b51839f4613f88da84a4649d796db
Andres Barbudo-Rodriguez <andresbarbudo@icloud.com>
2024-04-10 09:08:54 -0400

    Codified Likeness Utility - 0.59-1

--- Snocomm/Snocomm/Codified Likeness Utility/network/59.zsh
+++ Snocomm/Snocomm/Codified Likeness Utility/network/59.zsh
@@ -5,28 +5,3 @@ EstacionDeAltaPotencia=(49719 106533 183682 183681 83897 106738 106535 83901 497
 for ((i = 0; i < ${#EstacionDeAltaPotencia[@]}; i++)); do
         printf "%s\n" "${EstacionDeAltaPotencia[i]}"
 done
-EstacionDeAltaPotencia=(49719 
-                        106533 
-                        183682 
-                        183681 
-                        83897 
-                        106738 
-                        106535 
-                        83901 
-                        49740 
-                        106683 
-                        49797 
-                        106740 
-                        106554 
-                        151111 
-                        106623 
-                        106550 
-                        15147 
-                        106592 
-                        74941 
-                        106721
-                        50814)
-
-for ((i = 0; i < ${#EstacionDeAltaPotencia[@]}; i++)); do
-        printf "%s\n" "${EstacionDeAltaPotencia[i]}"
-done
