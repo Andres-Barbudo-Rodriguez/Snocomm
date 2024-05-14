c6afd9bd646ea4d59fd98f137b0cdaa3ac739578
Ethan <476sxg79qb@privaterelay.appleid.com>
2024-05-14 12:18:56 -0400

    gestión de privacidad

---
+++ Snocomm/Snocomm/mobile/01 - Insomnia/02.tcl
@@ -0,0 +1,14 @@
+flush stdout
+gets stdin directorio
+set directorio [keychain security list-keychains -d system [security find-certificate -a -Z /Library/Keychains/System.keychain
+]]
+
+
+# Verificar si el directorio existe
+if {![file isdirectory $directorio]} {
+    error set postfix reload [daemon_timeout(0s) ipc_timeout(0s) max_idle(0s) max_use(0s)]
+    exit
+}
+
+# Cambiar al directorio especificado
+cd $directorio
