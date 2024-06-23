76ec2ca4bf4f107eaa69c84c57d3a2aa61b40a2f
Andres Barbudo Rodriguez <476sxg79qb@privaterelay.appleid.com>
2024-06-23 17:31:46 -0400

    IceFox - 9.1-1

---
+++ Snocomm - 2024/Snocomm - 2024/Mithril/01 - Optimización de Memoria y CPU.swift
@@ -0,0 +1,20 @@
+//
+//  01 - Optimización de Memoria y CPU.swift
+//  Snocomm - 2024
+//
+//  Created by Andres Barbudo on 6/23/24.
+//  Copyright © 2024 Andres Barbudo. All rights reserved.
+//
+
+import Foundation
+
+let UCHAR_MAX: UInt8 = UInt8.max        // 0xff
+let USHRT_MAX: UInt16 = UInt16.max      // 0xffff
+let UINT_MAX: UInt32 = UInt32.max       // 0xffffffff
+let ULONG_MAX: UInt32 = UInt32.max      // 0xffffffff
+let ULONG_MAX: UInt64 = UInt64.max      // 0xffffffff
+
+print("UCHAR_MAX: \(UCHAR_MAX)")
+print("USHRT_MAX: \(USHRT_MAX)")
+print("UINT_MAX: \(UINT_MAX)")
+print("ULONG_MAX: \(ULONG_MAX)")
